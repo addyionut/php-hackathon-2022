f1b94e64566dd5708477862614e043d3cca0404a
Adrian Ionut Zaharia <addyionut@gmail.com>
2022-01-30 19:20:39 +0200

    Update db_connexion.php

--- db_connexion.php
+++ db_connexion.php
@@ -5,12 +5,12 @@
 	$password = "";
 	$db_users = "users";
 	$conn = mysqli_connect($servername,$username,$password, $db_users);
-	if(!$conn){
+	if (!$conn){
 		die("Connection failed".mysqli_connect_error());	
 	}
 	else {
-        echo "DB connected succesfully";
-    }	
-    echo "<br>";
+        	echo "DB connected succesfully";
+    	}	
+    	echo "<br>";
 	//mysqli_close($conn);	
 ?>
