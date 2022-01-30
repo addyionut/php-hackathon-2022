c0a1548205c8128f894b10b457cad906da9fa2c9
Adrian Ionut Zaharia <addyionut@gmail.com>
2022-01-30 12:04:09 +0200

    Add files via upload

---
+++ appointments_list.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<style>
+			table,th,td {
+				border: 1px solid black;
+			}
+		</style>
+	</head>
+	<body>
+		<?php
+			include 'server_conn.php';
+			$users_table = "SELECT * FROM `users_app`";
+			$result = mysqli_query($conn, $users_table);
+			if(mysqli_num_rows($result) > 0) {
+				echo"<table width=\"60%\" align=center><tr>
+					<th bgcolor=\"00FF00\">Name</th>	
+					<th bgcolor=\"00FF00\">CNP</th>
+					<th bgcolor=\"00FF00\">Activity/10</th>
+					<th bgcolor=\"00FF00\">Booking time</th>";
+				while($row=mysqli_fetch_assoc($result)) {
+					echo "<tr><td>".$row["username"]."</td>"
+							."<td>".$row["cnp"]."</td>"
+							."<td>".$row["activity"]."</td>"
+							."<td >".$row["meeting_time"]."</td>";
+				}
+				echo "</table>";
+			}
+			else {
+				echo "0 rows selected";
+			}
+			//mysqli_close($conexiune);
+		?>
+	</body>
+</html>
